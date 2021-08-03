6537a7dd82456da1ee065ead792921deacc74f03
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-04 04:14:18 +0530

    added new to calculate daily wage

--- employee_wage.py
+++ employee_wage.py
@@ -2,9 +2,13 @@ import random
 
 print("\tWelcome to Employee Wage Computation ")
 ATTENDANCE = random.randint(0, 1)
+DAY_HOURS = 8
+WAGE_PER_HOUR = 20
+DAILY_WAGE = 20 * 8
 
 if ATTENDANCE == 1:
     print("Employee is Present...........!")
+    print("Full Time employee Total day wage is : ", DAILY_WAGE)
 
 else:
     print("Employee is Absent.............!")
