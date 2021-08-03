d3e066b61586e826efa3eac65c0f599082c41401
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-04 04:07:35 +0530

    Added new feature to  check employee present or not

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,10 @@
-print("\tWelcome to Employee Wage Computation ") 
+import random
+
+print("\tWelcome to Employee Wage Computation ")
+ATTENDANCE = random.randint(0, 1)
+
+if ATTENDANCE == 1:
+    print("Employee is Present...........!")
+
+else:
+    print("Employee is Absent.............!")
