55e91e33226dc8f18c8405fa49de5cbebd8055d3
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-04 04:25:51 +0530

    added new feature to Slove case statement

--- employee_wage.py
+++ employee_wage.py
@@ -9,9 +9,14 @@ DAILY_WAGE = 20 * 8
 PART_TIME_WAGE = 20 * 4
 
 if ATTENDANCE == 1:
-    print("Employee is Present...........!")
-    print("Full Time employee Total day wage is : ", DAILY_WAGE)
-    print("Part Time employee Total day wage is : ", PART_TIME_WAGE)
+    emp_type = random.randint(0, 1)
+
+    if emp_type == 1:
+        print("full time Employee is Present...........!")
+        print("Full Time employee Total day wage is : ", DAILY_WAGE)
+    else:
+        print("part time Employee is Present...........!")
+        print("Part Time employee Total day wage is : ", PART_TIME_WAGE)
 
 else:
     print("Employee is Absent.............!")
