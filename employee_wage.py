e3321e0b3c4b81c4c9ab7db20c43339a395a4e1c
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-04 04:35:18 +0530

    added new feature to calculate monthly wage

--- employee_wage.py
+++ employee_wage.py
@@ -5,8 +5,11 @@ ATTENDANCE = random.randint(0, 1)
 DAY_HOURS = 8
 WAGE_PER_HOUR = 20
 PART_TIME_HOURS = 4
+WORK_DAYS = 20
 DAILY_WAGE = 20 * 8
 PART_TIME_WAGE = 20 * 4
+MONTHLY_FULL_WAGE = 20 * 20 * 8
+MONTHLY_PART_TIME_WAGE = 20 * 20 * 4
 
 if ATTENDANCE == 1:
     emp_type = random.randint(0, 1)
@@ -14,9 +17,11 @@ if ATTENDANCE == 1:
     if emp_type == 1:
         print("full time Employee is Present...........!")
         print("Full Time employee Total day wage is : ", DAILY_WAGE)
+        print("Full Time employee Total month wage is : ", MONTHLY_FULL_WAGE)
     else:
         print("part time Employee is Present...........!")
         print("Part Time employee Total day wage is : ", PART_TIME_WAGE)
+        print("Part Time employee Total month wage is : ", MONTHLY_PART_TIME_WAGE)
 
 else:
     print("Employee is Absent.............!")
