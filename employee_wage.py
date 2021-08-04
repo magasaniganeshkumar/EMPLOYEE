daccdcf6b45117de2ada4d80cfa9c66aa0e6b617
magasaniganeshkumar <83356306+magasaniganeshkumar@users.noreply.github.com>
2021-08-04 06:38:18 +0530

    added new feature to calculate Daiy and month Total wage

--- employee_wage.py
+++ employee_wage.py
@@ -14,13 +14,26 @@ MONTHLY_PART_TIME_WAGE = 20 * 20 * 4
 
 def get_hours():
 
+    daily_wage_full = 0
+    daily_wage_part = 0
+    present_count = 0
+    for day in range(1, 21):
+        random_number = random.randint(0, 1)
+        if random_number == 1:
+            present_count += 1
+    for i in range(present_count):
+        daily_wage_full += DAILY_WAGE
+        daily_wage_part += PART_TIME_WAGE
     if ATTENDANCE == 1:
         emp_type = random.randint(0, 1)
-        hours_type = random.randint(0, 1)
         break_hour = 0
         break_day = 0
         month_wage = 0
         if emp_type == 1:
+            cont = random.randint(0, 1)
+            for day in range(20):
+                if cont == 1:
+                    present_count += 1
             for day in range(20):
                 if break_day == 20 or break_hour >= 100:
                     print(" max hours or max day are reached ")
@@ -33,6 +46,7 @@ def get_hours():
             print("Full Time employee Total day wage is : ", DAILY_WAGE)
             print("Full Time employee Total month wage is : ", MONTHLY_FULL_WAGE)
             print(" full time employee work hours for a month is : ", DAY_HOURS * WORK_DAYS)
+            print("Full time employee present days total monthly wage is ", daily_wage_full)
 
         else:
             month_wage = 0
@@ -48,6 +62,7 @@ def get_hours():
             print("Part Time employee Total day wage is : ", PART_TIME_WAGE)
             print("Part Time employee Total month wage is : ", MONTHLY_PART_TIME_WAGE)
             print(" Part time employee work hours for a month is : ", PART_TIME_HOURS * WORK_DAYS)
+            print("part time employee present days total monthly wage is ", daily_wage_part)
 
     else:
         print("Employee is Absent.............!")
